98d0fa4a1248b8d63f8cbf0476d4008957359dc1
hyeongwoo <guddn0625@gmail.com>
2025-10-28 16:48:03 +0900

    fix: realEvents.json 諛섎났 ?쇱젙 ?쒓굅, ?듯빀 ?뚯뒪???ㅻ쪟 ?섏젙

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,11 @@ export default mergeConfig(
       environment: 'jsdom',
       setupFiles: './src/setupTests.ts',
       include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      poolOptions: {
+        threads: {
+          singleThread: true, // 통합 테스트 EMFILE 오류 방지
+        },
+      },
       coverage: {
         reportsDirectory: './.coverage',
         reporter: ['lcov', 'json', 'json-summary'],
